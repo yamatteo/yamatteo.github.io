9727e08b4fec9d96c54ca834a27b2471c3d2e060
Matteo Bortolotto <yamatteo@gmail.com>
2020-04-16 15:33:35 -0400

    Vue

--- js/app.js
+++
@@ -1,8 +0,0 @@
-function load_content(data) {
-    $("#"+data+" .showdiv").load( "content/"+data+".html");
-    $("#"+data+" .showa").removeAttr('onclick').click(retype);
-    setTimeout(retype, 200);
-}
-function retype() {
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-}
